b48d881dd2032ce3eda971d298bb9a08689cb7c1
MaryOleks <mariia.oleksiuk@omnigon.com>
2019-10-28 11:40:05 +0200

    Lesson 4 Task 1 - add router for news

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import { News } from '../Components/News';
-
-import newsData from '../data/news-data';
 import accordionData from '../data/accordion-data';
 import { Accordion } from '../Assets/Accordion';
 
 export const Home = () => {
   return (
     <>
-      <News source={ newsData } />
       <Accordion source={ accordionData } />
     </>
   )
